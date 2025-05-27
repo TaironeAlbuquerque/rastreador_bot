143b811be3a97f0ec1d2394612c11ed1fb676d07
Tairone Albuquerque <57819069+TaironeAlbuquerque@users.noreply.github.com>
2025-05-27 20:57:03 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('7304383872:AAH9jS7Vgix9TrgwjDWRBfg1ejgN6haik-0')
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 DATABASE_URL = 'sqlite:///rastreador.db'
